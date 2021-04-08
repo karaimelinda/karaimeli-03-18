a6c11cc6a173918538595286e2f44abb7bbf9d6a
karaimelinda <80882802+karaimelinda@users.noreply.github.com>
2021-04-08 08:46:38 +0200

    Lecke-04-08

--- main.ts
+++ main.ts
@@ -3,6 +3,16 @@ input.onButtonPressed(Button.A, function () {
     basic.pause(500)
     basic.showString("Szia!")
 })
+input.onGesture(Gesture.Shake, function () {
+    basic.clearScreen()
+    basic.pause(500)
+    véletlen = Math.randomBoolean()
+    if (véletlen == true) {
+        basic.showString("Igaz")
+    } else {
+        basic.showString("Hamis")
+    }
+})
 input.onButtonPressed(Button.AB, function () {
     basic.clearScreen()
     basic.pause(500)
@@ -13,6 +23,7 @@ input.onButtonPressed(Button.B, function () {
     basic.pause(500)
     basic.showNumber(2021)
 })
+let véletlen = false
 basic.showIcon(IconNames.Happy)
 basic.forever(function () {
 	
